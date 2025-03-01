ed19a2631661d9f5d6eedd5206a7b4353c8f17f5
Muhammad Khalil khaer <khalil.mn3@gmail.com>
2025-03-01 14:58:28 +0800

    ADD image upload utility with S3 integration

--- pkg/utils/image_upload.go
+++ pkg/utils/image_upload.go
@@ -135,3 +135,93 @@ func UploadImageUtility(file *multipart.FileHeader, UnitCode string) (filePath s
 	}
 	return endpoint + "/" + fileUrlPath, nil
 }
+
+func UploadImage(file *multipart.FileHeader, UnitCode string, FolderName string) (filePath string, err error) {
+	src, err := file.Open()
+	if err != nil {
+		return "", err
+	}
+
+	defer src.Close()
+
+	buf := bytes.NewBuffer(nil)
+	if _, err = io.Copy(buf, src); err != nil {
+		return "", err
+	}
+
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	var img image.Image
+	if ext == ".png" {
+		img, err = png.Decode(buf)
+	} else if ext == ".jpg" || ext == ".jpeg" {
+		img, err = jpeg.Decode(buf)
+	} else {
+		return "", fmt.Errorf("unsupported file format: %s", ext)
+	}
+	if err != nil {
+		return "", err
+	}
+
+	m := resize.Resize(512, 0, img, resize.Lanczos3)
+	path, imagePath, err := GetFilePath(UnitCode, "images/"+FolderName, FolderName)
+	if err != nil {
+		return "", err
+	}
+
+	filename := path + ".png"
+	imagePath += ".png"
+	out, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Encode and save the image as PNG
+	if err := png.Encode(out, m); err != nil {
+		out.Close()
+		return "", err
+	}
+	// write new image to file
+	if err := png.Encode(out, m); err != nil {
+		// master_data.SendResponse(global_var.ResponseCode.InvalidDataValue, fmt.Sprintf("upload file err: %s", err.Error()), nil, c)
+		out.Close()
+		return "", err
+	}
+
+	out.Close()
+	endpoint, s3Client, err := AwsLoad()
+	if err != nil {
+		fmt.Println("Error Initialize:", err.Error())
+		return "", err
+	}
+
+	// Reopen the file for uploading to S3
+	uploadFile, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	fileUrlPath := "pms-web/web-public" + imagePath
+	object := s3.PutObjectInput{
+		Bucket: aws.String("pms-storage"),
+		Key:    aws.String(fileUrlPath),
+		Body:   uploadFile,                // The object's contents.
+		ACL:    aws.String("public-read"), // Defines Access-control List (ACL) permissions, such as private or public.
+		Metadata: map[string]*string{ // Required. Defines metadata tags.
+			"x-amz-meta-type":      aws.String(FolderName),
+			"x-amz-meta-unit-code": aws.String(UnitCode),
+		},
+	}
+
+	// Step 5: Run the PutObject function with your parameters, catching for errors.
+	_, err = s3Client.PutObject(&object)
+	if err != nil {
+		fmt.Println(err.Error())
+		uploadFile.Close()
+		return "", err
+	}
+	uploadFile.Close()
+	// Delete the local file after successful upload
+	err = os.Remove(filename)
+	if err != nil {
+		return "", fmt.Errorf("failed to delete local file: %s", err)
+	}
+	return endpoint + "/" + fileUrlPath, nil
+}
